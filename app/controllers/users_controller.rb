fd1fb75fb0be8ec44e31c8ded50605aa4f49e558
Washer34 <antoine.paturel34@free.fr>
2023-06-15 00:09:09 +0200

    Users views + fix navbar + van_create + fix vans_users_id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
-  def index
-  end
 
   def show
-  end
-
-  def update
-  end
-
-  def edit
-  end
-  private
-
-  def set_user
     @user = User.find(params[:id])
   end
+  
 end
