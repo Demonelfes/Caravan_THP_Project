b5cb57ae69b6d0e3725a59c8e2758ecd4e852ba8
Adrien <ad.cresson@laposte.net>
2023-06-15 18:42:30 +0200

    dashboard admin index

--- db/schema.rb
+++ db/schema.rb
@@ -65,8 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_161812) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.boolean "is_admin", default: false
-    t.boolean "is_hidden", default: false
+    t.boolean "is_admin"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
