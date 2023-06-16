429de6361bd45dabb4a95f6131bf73feddd3c8c4
Laurie2023 <129828541+Laurie2023@users.noreply.github.com>
2023-06-16 19:10:38 +0200

    validation and flash V1

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,17 +1,34 @@
 class RentalsController < ApplicationController
+  add_flash_types :info, :error, :success
+
   def create
     @van = Van.find(params[:rental_id].to_i)
-    @rental = Rental.new(
-      'start_date' => params[:start],
-      'end_date' => params[:end],
-      'van_id' => @van.id,
-      'customer_id' => current_user.id,
-      'owner_id' => @van.user.id,
-      'total_price' => (params[:end].to_date - params[:start].to_date).to_i * @van.price_per_day)
+    puts "X"*30
+    puts params[:start].inspect
+    puts "X"*30
+    puts params[:end].inspect
+    puts "X"*30
+    if (params[:start] == "") || (params[:end] == "")
+      @rental = Rental.new(
+        'van_id' => @van.id,
+        'customer_id' => current_user.id,
+        'owner_id' => @van.user.id,
+        'total_price' => 1
+      )
+    else 
+      @rental = Rental.new(
+        'start_date' => params[:start],
+        'end_date' => params[:end],
+        'van_id' => @van.id,
+        'customer_id' => current_user.id,
+        'owner_id' => @van.user.id,
+        'total_price' => (params[:end].to_date - params[:start].to_date).to_i * @van.price_per_day)
+    end
     if @rental.save
       redirect_to van_path(@van, success: true)
     else
-      redirect_to van_path(@van, error: true)
+      flash[:alert] = @rental.errors.full_messages#.join(", ")
+      redirect_to van_path(@van)
     end
   end
 
