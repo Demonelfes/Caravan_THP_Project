42ecd298ba9e72c6d46e9d4df52029025a05df66
Washer34 <antoine.paturel34@free.fr>
2023-06-21 18:16:50 +0200

    integration_tests login logout registration rent a van; logo href fixed

---
+++ spec/features/logs_out_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "User logs out", type: :feature do
+  scenario "User logs out successfully" do
+    user = User.create(email: 'user@example.com', password: 'password')
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'user_password', with: 'password'
+    click_button "Se connecter"
+
+    click_link "Déconnexion"
+
+    expect(page).to have_content 'Connexion'
+  end
+end
---
+++ spec/features/rent_van_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Create Van", type: :feature do
+  scenario "User creates a new van" do
+    # Connecter l'utilisateur
+    user = User.create(email: 'user@example.com', password: 'password')
+    login_as(user, scope: :user)
+
+    # Visiter la page de création de van
+    visit new_van_path
+
+    # Remplir le formulaire
+    fill_in 'Titre', with: 'titre exemple'
+    fill_in 'Description', with: 'ceci est une description d\'exemple'
+    fill_in 'Immatriculation', with: 'AB-126-CD'
+    fill_in 'Marque', with: 'renault'
+    fill_in 'Ville', with: 'montpellier'
+    fill_in 'Année', with: '2020'
+    fill_in 'Carburant', with: 'essence'
+    fill_in 'Nombre de lits', with: '3'
+    fill_in 'Prix par jour', with: '300'
+    attach_file 'Photo', Rails.root.join('app', 'assets', 'images', 'default_van.jpg')
+
+    # Cliquer sur le bouton Soumettre
+    click_button 'Soumettre'
+
+    # Vérifier que le van a été ajouté à la liste des vans
+    expect(page).to have_content 'Le van a bien été enregistré.'
+    expect(Van.last.title).to eq 'titre exemple'
+    # Ajoutez d'autres assertions pour vérifier les autres attributs du van si nécessaire
+  end
+end
