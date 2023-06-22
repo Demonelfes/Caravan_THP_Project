f8c24383996c08e25f92011b8ed9335c32e93669
soja <solene.jabaud@gmail.com>
2023-06-22 13:05:22 +0200

    fix conflict

--- spec/controllers/vans_spec.rb
+++ spec/controllers/vans_spec.rb
@@ -22,17 +22,41 @@ RSpec.describe VansController, type: :controller do
   describe "GET show" do
     it "assigns @vans" do
       van = FactoryBot.create(:van)
-
-
       get :show, params: { id: van.id }
       expect(assigns(:van)).to eq(van)
     end
 
-  #   it "renders the show template" do
-  #     get :show, params: { id: van.id }
-
-  #     expect(response).to render_template("show")
-  #   end
+    it "renders the show template" do
+      van = FactoryBot.create(:van)
+      get :show, params: { id: van.id }
+      expect(response).to render_template("show")
+    end
   end
 
+## NEW ##
+
+  describe "GET new" do
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
+## EDIT ##
+
+  describe "GET edit" do
+    it "assigns @vans" do
+      van = FactoryBot.create(:van)
+      get :edit, params: { id: van.id }
+      expect(assigns(:van)).to eq(van)
+    end
+
+    it "renders the edit template" do
+      van = FactoryBot.create(:van)
+      get :edit, params: { id: van.id }
+      expect(response).to render_template("edit")
+    end
+  end
+
+  
 end
